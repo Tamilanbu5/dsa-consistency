2f8a604222f2d4466c1b09900473110472b12d56
Tamilanbu5 <tamilajay21@gmail.com>
2026-02-20 23:46:24 +0530

    Reverse Vowels

---
+++ arrays/ReverseVowels.java
@@ -0,0 +1,55 @@
+public class ReverseVowels {
+
+    static String reverse(String s){
+
+        char ch[]=s.toCharArray();
+
+        int start=0;
+
+        int end=s.length()-1;
+
+        while(start<end){
+
+            if(!isVowel(ch[start])){
+                start++;
+            }
+
+            else if(!isVowel(ch[end])){
+                end--;
+            }
+
+            else{
+                char temp=ch[start];
+                ch[start]=ch[end];
+                ch[end]=temp;
+
+                start++;
+                end--;
+            }
+        }
+
+        return new String(ch);
+
+
+    }
+
+    static boolean isVowel(char c){
+
+        if(c=='a'||c=='e'||c=='i'||c=='o'||c=='u'||c=='A'||
+        c=='E'||c=='I'||c=='O'||c=='U'){
+
+                   return true;
+        }
+
+        return false;
+
+    }
+
+    public static void main (String args[]){
+
+        String s="hello";
+
+        System.out.print(reverse(s));
+    }
+    
+}
