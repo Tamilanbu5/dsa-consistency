e7dc5c1e6896617dfea86e3674c7f73bd2a1f63a
Tamilanbu5 <tamilajay21@gmail.com>
2026-04-03 23:55:14 +0530

    Valid Palindrome 2

---
+++ arrays/ValidPalidromeII.java
@@ -0,0 +1,48 @@
+public class ValidPalidromeII{
+    public boolean validPalindrome(String s) {
+
+        int i=0;
+
+        int j=s.length()-1;
+
+        while(i<j){
+
+            if(s.charAt(i)==s.charAt(j)){
+
+                i++;
+
+                j--;
+            }
+
+            else{
+
+                return palindrome(s,i+1,j)||palindrome(s,i,j-1);
+            }
+        }
+
+        return true;
+        
+    }
+
+
+    boolean palindrome(String s,int i,int j){
+
+        while(i<j){
+
+            if(s.charAt(i)!=s.charAt(j)){
+
+            return false;
+        }
+
+        i++;
+        j--;
+
+        }
+
+        return true;
+
+        
+    }
+} 
+    
+
